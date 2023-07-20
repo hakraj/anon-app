12efc7176c7cc45e0f97bbe27e502c91ec6dd447
hakraj <adekunleddream@gmail.com>
2023-07-20 02:36:33 +0100

    checking out notify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,11 @@ import CreateArea from "./components/create_area";
 import AllNote from "./components/all_notes";
 import Footer from "./components/Footer";
 import Header from "./components/Navbar";
-import IsAuth from "./components/is_auth";
 
 
 const Home = () => {
   return (
-    <IsAuth>
+    <>
       <Header />
       <main>
         <Intro text="Hello, Love to Share Anon?" />
@@ -16,8 +15,7 @@ const Home = () => {
         <AllNote />
       </main>
       <Footer />
-    </IsAuth>
-
+    </>
   )
 }
 
