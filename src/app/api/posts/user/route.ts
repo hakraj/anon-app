ca1ce9276cbbc5210eae2b3e397901dd0a763631
hakraj <adekunleddream@gmail.com>
2023-08-08 01:10:23 +0100

    ok

--- src/app/api/posts/user/route.ts
+++ src/app/api/posts/user/route.ts
@@ -3,18 +3,20 @@ import dbConnect from "../../../../../lib/dbConnect";
 import Post from "../../../../../models/Post";
 import url from 'url';
 
-
+var z: { "author.email": string | string[] | undefined; };
 export async function GET(req: Request) {
   try {
     const user = url.parse(req.url, true).query;
 
     const doc = { "author.email": user.email }
 
+    z = doc
+
     await dbConnect()
 
     const posts = await Post.find(doc) /* find all the data in our database */
     return NextResponse.json({ success: true, data: posts })
   } catch (error) {
-    return NextResponse.json({ success: false })
+    return NextResponse.json({ success: false, re: z, error: error, })
   }
 }
--- src/app/components/main/account.tsx
+++ src/app/components/main/account.tsx
@@ -36,7 +36,7 @@ const Account = (
   return (
     <div className="flex justify-evenly items-center w-11/12 my-8 mx-auto lg:w-1/2 ">
       <div className="text-center px-[5%] " >
-        <h1 className="text-4xl sm:text-6xl">{notes && notes.length}</h1>
+        <h1 className="text-4xl sm:text-6xl">{notes ? notes.length : 0}</h1>
         <p className="text-xs sm:text-base text-slate-600 dark:text-slate-400">notes</p>
       </div>
       <div className="text-center px-[5%] ">
