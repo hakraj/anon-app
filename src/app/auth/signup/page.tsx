dcfc10ec3b0547a83a1eee588d07a805eccd568a
hakraj <adekunleddream@gmail.com>
2024-03-03 03:29:23 +0100

    fixes: intial prod

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -18,6 +18,7 @@ const Signup = () => {
   const { data: session, status } = useSession()
   const [credentials, setCredentials] = useState({ email: "", password: "" })
   const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
   const [viewPassword, setViewPassword] = useState(false)
 
   useEffect(() => {
@@ -32,6 +33,7 @@ const Signup = () => {
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault()
+    setLoading(true)
 
     if (credentials.password.length >= 8) {
       const res = await PostUser(credentials);
@@ -39,19 +41,22 @@ const Signup = () => {
       if (res.sucesss) {
         const user = await signIn("credentials", { ...credentials, redirect: false })
         if (user?.error === null) {
+          setLoading(false)
           router.push('/auth/onboarding/username')
         } else {
           const err = user?.error as string;
-          console.log(err);
-          setError("invalid credential")
+          setLoading(false)
+          setError("invalid credentials")
         }
       } else {
         console.log(res.error);
-        setError("invalid credential")
-        throw new Error("invalid credential")
+        setLoading(false)
+        setError("something went wrong")
+        throw new Error("something went wrong")
       }
     } else {
-      setError(" password cannot be less than 8 characters ")
+      setLoading(false)
+      setError("password cannot be less than 8 characters ")
     }
   }
 
@@ -101,18 +106,18 @@ const Signup = () => {
                 value={credentials.password}
               />
               {!viewPassword ?
-                <svg onClick={() => setViewPassword(true)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" text-[#f5ba13] absolute right-4 bottom-2 w-5 h-5">
+                <svg onClick={() => setViewPassword(true)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" text-slate-300 dark:text-slate-500 hover:text-[#f5ba13] absolute right-4 bottom-2 w-5 h-5">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
                 :
-                <svg onClick={() => setViewPassword(false)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" text-[#f5ba13] absolute right-4 bottom-2 w-5 h-5">
+                <svg onClick={() => setViewPassword(false)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" text-slate-300 dark:text-slate-500 hover:text-[#f5ba13] absolute right-4 bottom-2 w-5 h-5">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
                 </svg>
               }
             </div>
             {error && <ErrorMessage text={error} />}
-            <FormBtn error={!error} text="Sign up" />
+            <FormBtn loading={loading} text="Login" />
 
             <p className="text-xs text-slate-400 dark:text-slate-600 my-4">or</p>
 
